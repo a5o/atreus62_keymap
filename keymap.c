8531cee53779362acee8c8068a8ab303ee2b31ab
a5o <alberto.ferrarini@gmail.com>
2020-11-21 10:41:38 +0100

    renamed NAV layer to FUN

--- keymap.c
+++ keymap.c
@@ -11,7 +11,7 @@
 #define _BASE 0
 #define _LOWER 1
 #define _RAISE 2
-#define _NAV 3
+#define _FUN 3
 #define _ADJUST 4
 
 enum custom_keycodes {
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL ,
     CTL_T(KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT ,
     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_GRV ,
-    MO(_NAV),KC_LCTL,   KC_LALT, KC_LGUI,LOWER,  KC_BSPC, KC_DEL,   KC_ENT,   KC_SPC,  RAISE,  KC_LBRC, KC_RBRC , KC_PGUP , KC_PGDN
+    MO(_FUN),KC_LCTL,   KC_LALT, KC_LGUI,LOWER,  KC_BSPC, KC_DEL,   KC_ENT,   KC_SPC,  RAISE,  KC_LBRC, KC_RBRC , KC_PGUP , KC_PGDN
   ),
 
   [_LOWER] = LAYOUT(
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
-  [_NAV] = LAYOUT(
+  [_FUN] = LAYOUT(
     KC_TRNS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11  ,
     KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
     KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS ,
