3c0640750171b5966efba8300c566324d33387c7
a5o <alberto.ferrarini@gmail.com>
2020-11-22 12:50:31 +0100

    Changed position of pipe vs tilde

--- keymap.c
+++ keymap.c
@@ -26,22 +26,22 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  /* Qwerty
  * ,-------------------------------------------------------------------------------------------------.
- * |  \|  |   1! |   2@ |   3# |   4$ |   5% |      |      |   6^ |   7& |   8* |   9( |   0) |  -_  |
+ * |  `~  |   1! |   2@ |   3# |   4$ |   5% |      |      |   6^ |   7& |   8* |   9( |   0) |  -_  |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |      |      |   Y  |   U  |   I  |   O  |   P  |  =+  |
  * |------+------+------+------+------+-------------+------+------+------+------+------+------+------|
  * | Esc  |   A  |   S  |   D  |   F  |   G  |      |      |   H  |   J  |   K  |   L  |  ;:  |  '"  |
  * |------+------+------+------+------+------|------+------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |      |      |   N  |   M  |  ,<  |  .>  |  /?  |  `~  |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |      |      |   N  |   M  |  ,<  |  .>  |  /?  |  \|  |
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
  * | Fn   | Ctrl | Alt  | GUI  |LOWER |Delete| Canc |Enter |Space |RAISE |  [{  |  ]}  | PgUp | PgDn |
  * `-------------------------------------------------------------------------------------------------'
  */
  [_BASE] = LAYOUT( /* qwerty */
-    KC_BSLS,       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS ,
+    KC_GRV,       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS ,
     KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL ,
     CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT ,
-    KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_GRV ,
+    KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS ,
     MO(_FUN),      KC_LCTL,   KC_LALT, KC_LGUI,LOWER,  KC_BSPC, KC_DEL,   KC_ENT,   KC_SPC,  RAISE,  KC_LBRC, KC_RBRC , KC_PGUP , KC_PGDN
   ),
 
