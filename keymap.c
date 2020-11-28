3c44eef8dfce9a3ae3a2c55e3b15bca543e8f804
a5o <alberto.ferrarini@gmail.com>
2020-11-28 14:43:26 +0100

    minor changes

--- keymap.c
+++ keymap.c
@@ -37,20 +37,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Fn   | Ctrl | Alt  | GUI  |LOWER |Delete| Canc |Enter |Space |RAISE |  [{  |  ]}  | PgUp | PgDn |
  * `-------------------------------------------------------------------------------------------------'
  */
- [_BASE] = LAYOUT( /* qwerty */
-    KC_GRV,       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS ,
+//[_BASE] = LAYOUT( /* qwerty */
+//    KC_GRV,       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS ,
+//    KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL ,
+//    CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT ,
+//    KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS ,
+//    MO(_FUN),      KC_LCTL,   KC_LALT, KC_LGUI,LOWER,  KC_BSPC, KC_DEL,   KC_ENT,   KC_SPC,  RAISE,  KC_LBRC, KC_RBRC , KC_PGUP , KC_PGDN
+//  ),
+
+[_BASE] = LAYOUT( /* qwerty */
+    KC_BSLS,       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS ,
     KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL ,
     CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT ,
-    KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS ,
-    MO(_FUN),      KC_LCTL,   KC_LALT, KC_LGUI,LOWER,  KC_BSPC, KC_DEL,   KC_ENT,   KC_SPC,  RAISE,  KC_LBRC, KC_RBRC , KC_PGUP , KC_PGDN
+    KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+    MO(_FUN),      KC_LCTL,   KC_LALT, KC_LGUI,LOWER,  KC_BSPC, KC_DEL,   KC_SPC,   KC_GRV,  RAISE,  KC_LBRC, KC_RBRC , KC_PGUP , KC_PGDN
   ),
 
+
+
   [_LOWER] = LAYOUT(
-    KC_TILD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC ,
-    KC_TRNS,  KC_TRNS, KC_TRNS, IT_EGRV, KC_TRNS, KC_TRNS,                     KC_TRNS, IT_UGRV, IT_IGRV, IT_OGRV, KC_TRNS, KC_TRNS ,
-    KC_TRNS,  IT_AGRV, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
+    KC_PIPE,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS ,
+    KC_TRNS,  KC_TRNS, KC_TRNS, IT_EGRV, KC_TRNS, KC_TRNS,                     KC_TRNS, IT_UGRV, IT_IGRV, IT_OGRV, KC_TRNS, KC_PLUS ,
+    KC_TRNS,  IT_AGRV, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DQT ,
     KC_LCAP,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
-    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_END
   ),
 
   [_RAISE] = LAYOUT(
@@ -66,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
     KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_TRNS, KC_TRNS ,
     KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
-    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_END
+    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
   [_ADJUST] = LAYOUT(
